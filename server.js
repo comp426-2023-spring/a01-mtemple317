d7308cbd024aee97556d6c7c2f84a7a54839526f
Michelle Temple <michelletemple@MacBook-Pro-79.local>
2023-01-23 20:38:32 -0500

    initial commit of all files for assignment 1

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
+const http = require('node:http');
 // Require http module
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
 // Require fs module
 
