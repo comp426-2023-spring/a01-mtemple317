e2605c68e3d71996e9881ca568c4d4c57c5b2a9d
Michelle Temple <michelletemple@macbook-pro-79.wireless-1x.unc.edu>
2023-02-02 15:13:24 -0500

    updating file with minimist info

--- server.js
+++ server.js
@@ -1,40 +1,41 @@
-
-// Require http module
-
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-// Require fs module (read in ./public/index.html and put that inside res.end instead of Hello World).
-const fs = require('fs');
-
-fs.readFile('./public/index.html', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(data);
-});
-
 // Require minimist module (make sure you install this one via npm).
 
 const minimist = require('minimist');
 
-let args = minimist(process.argv.slice(2), {
-    default: {
-        port: 3000
-    },
+const args = minimist(process.argv.slice(2))
+ console.log("command line arguments: ")
+ console.log(args)
+ console.log("what is in args port?: ")
+ console.log(args.port)
+console.log ('args:',args); 
+
+// Require http module
+
+const http = require('http');
+const hostname = args.hostname|| '127.0.0.1';
+const port = args.port || 3000;
+const fs = require('fs')
+fs.readFile('./public/index.html', 'utf8', (err, data) => {
+   if (err) {
+	console.error(err);
+	return;
+	} else {
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(data);
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server listening on port ${port}`);
+});
+  };
 });
 
-console.log ('args:',args); 
+// Require fs module (read in ./public/index.html and put that inside res.end instead of Hello World).
+
+
+
 
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
 
