737e26cbe08a591471ac2439e78273f686450bce
Michelle Temple <michelletemple@MacBook-Pro-79.local>
2023-02-04 16:57:53 -0500

    changed content typeto html instead of plain

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 	} else {
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/html');
   res.end(data);
 });
 server.listen(port, hostname, () => {
